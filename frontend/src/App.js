ceeb7290f428e4b0f477e9c1847c8d95ac4d77db
Jaitra Saketh <140096487+jaitrasaketh@users.noreply.github.com>
2024-06-11 22:40:16 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
