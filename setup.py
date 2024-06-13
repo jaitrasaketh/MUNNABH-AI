f6b1be27485aa84e4580b4ca0a6c2e88166d29aa
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-06-13 17:28:33 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Munnabh-AI',
+    version = '0.0.0',
+    author = ['Harshith Reddy',
+               'Jaitra Saketh'],
+    packages = find_packages(),
+    install_requires = []
+
+)
