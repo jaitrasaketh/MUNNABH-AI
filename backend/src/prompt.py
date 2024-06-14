081f06eebb1cab0066e187aeae3a653352e1e381
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-06-14 21:45:40 +0530

    Prompt template update

--- backend/src/prompt.py
+++ backend/src/prompt.py
@@ -1,19 +1,35 @@
-diagnosis_template = """
-Use the following pieces of information to give the diagnosis for the symptoms presented.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+class diagnosis:
+    template = """
+        You are a doctor and a patient comes to you with the following symptoms:
+        Use the following pieces of information to give the diagnosis for the symptoms presented.
+        If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
-Context: {context}
+        Context: {context}
 
-Only return the helpful answer below and nothing else.
-Helpful answer:
-"""
+        Only return the helpful answer below and nothing else.
+        Helpful answer:
+        """
 
-prognosis_template = """
-Use the following pieces of information to give the prognosis for the given disease and symptoms.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+class prognosis:
+        template = """
+        You are a doctor and a patient comes to you with the following diagnosis:
+        Use the following pieces of information to give the prognosis for the given disease and symptoms.
+        If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
-Context: {context}
+        Context: {context}
 
-Only return the helpful answer below and nothing else.
-Helpful answer:
-"""
+        Only return the helpful answer below and nothing else.
+        Helpful answer:
+        """
+
+class ask:
+    template = """
+        You are a doctor and a patient comes to you with the following question:
+        Use the following pieces of information to give the prognosis for the given disease and symptoms.
+        If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+        Context: {context}
+
+        Only return the helpful answer below and nothing else.
+        Helpful answer:
+        """
