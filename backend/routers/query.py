8a6f28446de4dbc79497184e44069bd470460171
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-06-16 11:59:22 +0530

    edit

--- backend/routers/query.py
+++ backend/routers/query.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI, Response, status, HTTPException, APIRouter, Depends
+from fastapi import status, HTTPException, APIRouter
 from .. import schemas
 from ..llm_service import LLMService
+
+# To load the tensorflow model
+from fastapi import UploadFile, File
+from fastapi.responses import JSONResponse
+import tensorflow as tf
+from ..cnn_service import CNNService
+
+router = APIRouter()
+
+cnn_service = CNNService()
+cnn_service.load_models()
 
 router = APIRouter(prefix="/query", tags=["query"])
 llm_service = LLMService()
@@ -13,6 +24,14 @@ def chat(query: schemas.Query):
     result = llm_service.get_mistral(formatted_template)
     return {"output": result}
 
+@router.post("/classify_xray")
+async def classify_xray(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = tf.image.decode_image(contents, channels=3)
+    predictions = cnn_service.classify_xray(image)
+    labels = ['Cardiomegaly', 'Emphysema', 'Effusion', 'Hernia', 'Infiltration', 'Mass', 'Nodule', 'Atelectasis', 'Pneumothorax', 'Pleural_Thickening', 'Pneumonia', 'Fibrosis', 'Edema', 'Consolidation']
+    result = {label: float(pred) for label, pred in zip(labels, predictions[0])}
+    return JSONResponse(content=result)
 
 @router.post("/select/{template_name}", status_code=status.HTTP_200_OK)
 def select_template(template_name: str):
