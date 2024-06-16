e1b029fb2b6c0e28797c9f16f387ca95eb448c49
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-06-16 11:43:16 +0530

    API key change

--- backend/config.py
+++ backend/config.py
@@ -4,5 +4,6 @@ import os
 load_dotenv()
 
 class Config:
+    MISTRAL_API_KEY = os.getenv('MISTRAL_API_KEY')
     PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
     MODEL_PATH = "backend/model/phi-2.Q4_0.gguf"
