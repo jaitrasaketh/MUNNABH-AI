bcbd4d1bb0a4cc59b9b2cabb3355fa6a78cf5eb6
reddy-j-harshith <f20220233@hyderabad.bits-pilani.ac.in>
2024-06-17 23:14:17 +0530

    Edit

--- backend/routers/query.py
+++ backend/routers/query.py
@@ -17,18 +17,27 @@ cnn_service.load_models()
 router = APIRouter(prefix="/query", tags=["query"])
 llm_service = LLMService()
 
+def extract_values(data):
+    result = []
+    for key, entries in data.items():
+        for entry in entries:
+            result.append(f"{key}: {entry['value']}")
+    return '\n'.join(result)
+
 def get_google_fit_data():
-    url = "https://v1.nocodeapi.com/unlimited_power/fit/UAmylmNVsVXkcjWT/aggregatesDatasets?dataTypeName=steps_count,active_minutes,calories_expended,heart_minutes,sleep_segment,weight,activity_summary"
+    url = "https://v1.nocodeapi.com/unlimited_power/fit/UAmylmNVsVXkcjWT/aggregatesDatasets?dataTypeName=steps_count,active_minutes,calories_expended,heart_minutes,sleep_segment,weight"
     params = {}
     r = requests.get(url=url, params=params)
-    return r.json()
+    formatted_string = extract_values(r.json())
+    return formatted_string
 
 @router.post("/message", status_code=status.HTTP_201_CREATED)
 def chat(query: schemas.Query):
     similar_docs = llm_service.vector_search(query.query)
     template = llm_service.get_prompt_template()
     google_fit_data = get_google_fit_data()
-    formatted_template = template.format(similar_docs=similar_docs, query=query.query, google_fit_data=google_fit_data)  
+    formatted_template = template.format(similar_docs=similar_docs, query=query.query)  
+    formatted_template += google_fit_data
     result = llm_service.get_mistral(formatted_template)
     return {"output": result}
 
